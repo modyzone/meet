c22ae2b9e46ca01e4775cb037100a4f2d8013813
Modyzone <mody.zone@outlook.com>
2021-12-10 14:23:08 +0100

    add new files

---
+++ src/EventList.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Event from './Event';
+
+class EventList extends Component {
+    render() {
+        const { events } = this.props;
+       
+        return (
+            <ul className="EventList">
+                {events.map(event =>
+                    <li key={event.id} className="event">
+                        <Event event={event} />
+                    </li>
+                )}
+            </ul>
+        );
+    }
+}
+
+export default EventList;
