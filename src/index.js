6b39d4525bd474ef787963fb6b69a900f0bb7269
Modyzone <mody.zone@outlook.com>
2021-12-25 16:29:27 +0200

    add new files

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+atatus.config('7ad5791bd9b440b6870845eefbcad664').install();
 
 ReactDOM.render(
   <React.StrictMode>
@@ -21,3 +23,4 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+atatus.notify(new Error('Test Atatus Setup'));
