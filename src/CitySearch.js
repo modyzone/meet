94461225a1a5b6f936b676ad00bfc65bbd5692be
Modyzone <mody.zone@outlook.com>
2021-12-28 18:45:56 +0200

    add new files

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { InfoAlert } from './Alert';
 
 class CitySearch extends Component {
     state = {
@@ -8,22 +9,31 @@ class CitySearch extends Component {
     }
     handleInputChanged = (event) => {
         const value = event.target.value;
+        this.setState({ showSuggestions: true });
         const suggestions = this.props.locations.filter((location) => {
             return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
         });
-        this.setState({
-            query: value,
-            suggestions
-        });
-    };
-        handleItemClicked = (suggestion) => {
+        if (suggestions.length === 0) {
             this.setState({
-                query: suggestion,
-                showSuggestions: false
+                query: value,
+                infoText: 'We can not find the city you are looking for. Please try another city',
             });
-            this.props.updateEvents(suggestion);
-        };
-       
+        } else {
+            return this.setState({
+                query: value,
+                suggestions,
+                infoText: ''
+            });
+        }
+    };
+    handleItemClicked = (suggestion) => {
+        this.setState({
+            query: suggestion,
+            showSuggestions: false
+        });
+        this.props.updateEvents(suggestion);
+    };
+
     render() {
         return (
             <div className="CitySearch">
@@ -34,7 +44,7 @@ class CitySearch extends Component {
                     onChange={this.handleInputChanged}
                     onFocus={() => { this.setState({ showSuggestions: true }) }}
                 />
-                <ul className="suggestions" style={this.state.showSuggestions ? {}: { display: 'none' }}>
+                <ul className="suggestions" style={this.state.showSuggestions ? {} : { display: 'none' }}>
                     {this.state.suggestions.map((suggestion) => (
                         <li key={suggestion}
                             onClick={() => this.handleItemClicked(suggestion)}>{suggestion}</li>
