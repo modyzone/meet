c741b823521969f889dea23492a69fbe3bf5fe1f
Modyzone <mody.zone@outlook.com>
2021-12-30 17:31:49 +0200

    add new files

--- src/Event.js
+++ src/Event.js
@@ -3,10 +3,13 @@ import React, { Component } from "react";
 class Event extends Component {
 
   render() {
-
+      const { event } = this.props;
+      console.log(event);
     return (
-
       <div>
+        <ul>
+          <li>{event.location}</li>
+        </ul>
       </div>
     );
   }
