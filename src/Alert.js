ec823d3dd7d722a86bebfdf707b378d045bc47ae
Modyzone <mody.zone@outlook.com>
2021-12-30 15:33:15 +0200

    add new files

--- src/Alert.js
+++ src/Alert.js
@@ -8,53 +8,38 @@ class Alert extends Component {
 
   getStyle = () => {
     return {
-      color: this.color,
+      color: this.color
     };
   }
 
   render() {
     return (
       <div className="Alert">
-        <p style={this.getStyle()}>{this.props.text}</p>
+        <p style={this.getStyle()} className="error-text">{this.props.text}</p>
       </div>
     );
   }
 }
+
 class InfoAlert extends Alert {
-    constructor(props) {
-      super(props);
-      this.color = 'blue';
-    };
-  
-  render() {
-    return (
-      <div className="CitySearch">
-        <InfoAlert text={this.state.infoText} />
-        ...
-      </div>
-  );
-  };
+  constructor(props) {
+    super(props);
+    this.color = 'yellow';
+  }
 }
 
-class ErrorAlert extends Component {
-    constructor(props) {
+class ErrorAlert extends Alert {
+  constructor(props) {
     super(props);
     this.color = 'red';
   }
-
-getStyle = () => {
-   return {
-     color: this.color,
-    };
-   }
-render() {
- return (
- <div className="Alert">
-   <p style={this.getStyle()}>{this.props.text}</p>
-  </div>
-);
-}
 }
 
-  export { InfoAlert };
-  export { ErrorAlert };
+class WarningAlert extends Alert {
+  constructor(props) {
+    super(props);
+    this.color = 'orange';
+  }
+}
+
+export { InfoAlert, ErrorAlert, WarningAlert };
---
+++ src/features/showHideEventDetails.feature
@@ -0,0 +1,13 @@
+Feature: Show/hide an event details
+
+
+Scenario : An event element is collapsed by default.
+   Given An event element is collapsed by default
+
+Scenario : User can expand an event to see its details.
+  Given User can expand an event to see its details
+
+Scenario : User can collapse an event to hide its details.
+   Given User can collapse an event to hide its details
+
+  
