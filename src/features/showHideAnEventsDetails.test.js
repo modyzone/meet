5f582207caf734437fcc873707b405c03131345a
Modyzone <mody.zone@outlook.com>
2022-01-03 16:30:45 +0200

    add new files

--- src/features/showHideAnEventsDetails.test.js
+++ src/features/showHideAnEventsDetails.test.js
@@ -1,45 +1,54 @@
-import puppeteer from 'puppeteer';
+import { loadFeature, defineFeature } from "jest-cucumber";
+import React from 'react';
+import { mount, shallow } from 'enzyme';
 
-import { loadFeature, defineFeature } from 'jest-cucumber';
+import { mockData } from "../mock-data";
+import { extractLocations } from "../api";
+const locations = extractLocations(mockData);
 
-const feature = loadFeature('./src/features/showHidEventDetails.feature');
+import App from '../App';
+import CitySearch from "../CitySearch";
+
+// loadFeature expects file path to start from project root
+const feature = loadFeature('./src/features/filterEventsByCity.feature');
 
 defineFeature(feature, test => {
-});
+    
+    test('User should see a list of suggestions when they search for a city', ({ given, when, then }) => {
+        given('the main page is open', () => {
 
-describe('show/hide an event details', () => {
-    let browser;
-    let page;
-    beforeAll(async () => {
-        jest.setTimeout(30000);
-        const browser = await puppeteer.launch({
-            headless: false,
-            slowMo: 250, // slow down by 250ms
-            ignoreDefaultArgs: ['--disable-extensions'] // ignores default setting that causes timeout errors
         });
-        page = await browser.newPage();
-        await page.goto('http://localhost:3000/');
-        await page.waitForSelector('.event');
+
+        when('the user starts typing in the city textbox', () => {
+
+        });
+
+        then('the user should receive a list of cities (suggestions) that match what they’ve typed', () => {
+
+        });
     });
 
-    afterAll(() => {
-        browser.close();
-    });
+     "User can select a city from the suggested list",
 
-    test('An event element is collapsed by default', async () => {
-        const eventDetails = await page.$('.event .event__Details');
-        expect(eventDetails).toBeNull();
-    });
+    test('User can select a city from the suggested list', ({ given, and, when, then }) => {
+        given('user was typing “Berlin” in the city textbox', () => {
 
-    test('User can expand an event to see its details', async () => {
-        await page.click('.event .details-btn');
-        const eventDetails = await page.$('.event .event__Details');
-        expect(eventDetails).toBeDefined();
-    });
+        });
 
-    test('User can collapse an event to hide its details', async () => {
-        await page.click('.event .details-btn');
-        const eventDetails = await page.$('.event .event__Details');
-        expect(eventDetails).toBeNull();
+        and('the list of suggested cities is showing', () => {
+
+        });
+
+        when('the user selects a city (e.g., “Berlin, Germany”) from the list', () => {
+
+        });
+
+        then('their city should be changed to that city (i.e., “Berlin, Germany”)', () => {
+
+        });
+
+        and('the user should receive a list of upcoming events in that city', () => {
+
+        });
     });
 });
--- src/features/showHideEventDetails.feature
+++ src/features/showHideEventDetails.feature
@@ -1,13 +1,16 @@
-Feature: Show/hide an event details
+Feature: Show/hide and event's details
 
+  Scenario: When the user has not clicked on an event, each event element should be collapsed.
+    Given the main page is open
+    When the user has not clicked on an event
+    Then each event element should be collapsed.
 
-Scenario : An event element is collapsed by default.
-   Given An event element is collapsed by default
+  Scenario: When the user clicks on a collapsed event element, the element should expand.
+    Given an event element is collapsed
+    When the user clicks on an event
+    Then the event element should expand.
 
-Scenario : User can expand an event to see its details.
-  Given User can expand an event to see its details
-
-Scenario : User can collapse an event to hide its details.
-   Given User can collapse an event to hide its details
-
-  
+  Scenario: When the user clicks on an expanded event element, the element should collapse.
+    Given an event element is expanded
+    When the user clicks on an event
+    Then the event element should collapse.
