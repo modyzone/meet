ffc79d6373ed9627925fcd7f90a2dc16bd040150
Modyzone <mody.zone@outlook.com>
2022-01-05 17:25:56 +0200

    add new files

--- src/features/showHideAnEventsDetails.test.js
+++ src/features/showHideAnEventsDetails.test.js
@@ -51,4 +51,17 @@ defineFeature(feature, test => {
 
         });
     });
+    test('When user hasn’t searched for a city, show upcoming events from all cities.', ({ given, when, then }) => {
+        given('user hasn’t searched for any city', () => {
+
+        });
+
+        when('the user opens the app', () => {
+
+        });
+
+        then('the user should see the list of upcoming events.', () => {
+
+        });
+    });
 });
