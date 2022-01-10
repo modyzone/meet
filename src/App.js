64bff01ddcbc71eb1635ee9e885a9e689e082d86
modyzone <mody.kassem@gmail.com>
2022-01-10 13:20:35 +0200

    add new files

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import { extractLocations, getEvents } from './api';
+import {
+  ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer
+} from 'recharts';
 
 class App extends Component {
   state = {
@@ -21,10 +24,35 @@ class App extends Component {
     this.mounted = false;
   }
   render() {
+    const { locations, numberOfEvents, events } = this.state;
     return (
       <div className="App">
-        <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
-        <EventList events={this.state.events} />
+        <h1>Meet App</h1>
+        <h4>Choose your nearest city</h4>
+        <CitySearch updateEvents={this.updateEvents} locations={locations} />
+        <NumberOfEvents
+          updateEvents={this.updateEvents}
+          numberOfEvents={numberOfEvents}
+        />       
+         <div className="data-vis-wrapper">
+           <EventGenre events={events} />
+           <ResponsiveContainer height={400} >
+             <ScatterChart margin={{ top:20, right:20, bottom: 20, left:20 }}>
+         <h4>Events in each city</h4>
+         <CitySearch updateEvents={this.updateEvents} locations={locations} />
+            <CartesianGrid />
+            <XAxis type="category" dataKey="city" name="city" />
+            <YAxis
+              allowDecimals={false}
+              type="number"
+              dataKey="number"
+              name="number of events"
+            />
+            <Tooltip cursor={{ strokeDasharray: "3 3" }} />
+            <Scatter data={this.getData()} fill="#8884d8" />
+          </ScatterChart>
+        </ResponsiveContainer>
+        <EventList events={events} />
       </div>
     );
   }
@@ -38,6 +66,15 @@ class App extends Component {
       });
     });
   }
+  getData = () => {
+    const {locations, events} = this.state;
+    const data = locations.map((location)=>{
+      const number = events.filter((event) => event.location === location).length
+      const city = location.split(', ').shift()
+      return {city, number};
+    })
+    return data;
+  };
 }
 
 export default App;
